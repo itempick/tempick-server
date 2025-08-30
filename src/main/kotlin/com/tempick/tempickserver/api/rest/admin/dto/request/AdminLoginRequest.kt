5fd9bcbac771e6d3a8f72988dd12b0e98b6ad5fc
bread <thayer2392@gmail.com>
2025-08-30 20:45:15 +0900

    스웨거 수정

--- src/main/kotlin/com/tempick/tempickserver/api/rest/admin/dto/request/AdminLoginRequest.kt
+++ src/main/kotlin/com/tempick/tempickserver/api/rest/admin/dto/request/AdminLoginRequest.kt
@@ -3,8 +3,8 @@ package com.tempick.tempickserver.api.rest.admin.dto.request
 import io.swagger.v3.oas.annotations.media.Schema
 
 data class AdminLoginRequest (
-    @Schema(description = "관리자 로그인 아이디", example = "privateadmin")
+    @field:Schema(description = "관리자 로그인 아이디", example = "privateadmin")
     val loginId: String,
-    @Schema(description = "관리자 로그인 비밀번호", example = "admin12!!@")
+    @field:Schema(description = "관리자 로그인 비밀번호", example = "admin12!!@")
     val password: String
 )
